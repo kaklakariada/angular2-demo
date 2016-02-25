839652a0cdccb3f4472c7cd443a2c5be35efc1b2
kaklakariada <kaklakariada@users.noreply.github.com>
2016-02-25 19:41:35 +0100

    Fix tic compile errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 const gulp = require('gulp');
 const del = require('del');
 const typescript = require('gulp-typescript');
-const tscConfig = require('./tsconfig.json');
 const sourcemaps = require('gulp-sourcemaps');
+
 
 // clean the contents of the distribution directory
 gulp.task('clean', function() {
@@ -11,12 +11,12 @@ gulp.task('clean', function() {
 
 // TypeScript compile
 gulp.task('compile', ['clean'], function() {
-  return gulp
-    .src('app/**/*.ts')
+  var tsProject = typescript.createProject('tsconfig.json');
+  return tsProject.src()
     .pipe(sourcemaps.init())
-    .pipe(typescript(tscConfig.compilerOptions))
+    .pipe(typescript(tsProject))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist/app'));
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('build', ['compile']);
