2fd7f99b03519dcb9b8ff4d9588f16d212cac359
kaklakariada <kaklakariada@users.noreply.github.com>
2016-02-27 08:11:50 +0100

    Copy dependencies to dist/lib

--- gulpfile.js
+++ gulpfile.js
@@ -19,11 +19,24 @@ gulp.task('compile', ['clean'], function() {
     .pipe(gulp.dest('dist'));
 });
 
+// copy dependencies
+gulp.task('copy:libs', ['clean'], function() {
+  return gulp.src([
+      'node_modules/es6-shim/es6-shim.min.js',
+      'node_modules/systemjs/dist/system-polyfills.js',
+      'node_modules/angular2/bundles/angular2-polyfills.js',
+      'node_modules/systemjs/dist/system.src.js',
+      'node_modules/rxjs/bundles/Rx.js',
+      'node_modules/angular2/bundles/angular2.dev.js'
+    ])
+    .pipe(gulp.dest('dist/lib'))
+});
+
 gulp.task('tslint', function() {
   return gulp.src('app/**/*.ts')
     .pipe(tslint())
     .pipe(tslint.report('verbose'));
 });
 
-gulp.task('build', ['compile', 'tslint']);
+gulp.task('build', ['compile', 'copy:libs', 'tslint']);
 gulp.task('default', ['build']);
